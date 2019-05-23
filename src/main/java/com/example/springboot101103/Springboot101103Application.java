451d36fc9bef182e175623fb039e23b97e5faf84
Jacob Lefler <jcb.lefler@gmail.com>
2019-05-23 11:26:35 -0400

    initial commit

---
+++ src/main/java/com/example/springboot101103/Springboot101103Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot101103;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot101103Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot101103Application.class, args);
+	}
+
+}
